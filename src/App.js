8cfa0816467a37e4410b77ee3a5397533020da37
andylidong <andy_lidong@foxmail.com>
2019-12-27 12:04:50 +0800

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This is a demo for test auto deploy!
+          just a demo for test auto pull code and test deploy
         </p>
       </header>
     </div>
