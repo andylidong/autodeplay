ed6c57c742006a877226ca594cbc7c5cb34e0d8f
andylidong <andy_lidong@foxmail.com>
2019-12-27 11:18:03 +0800

    demo for test auto pull code and test deploy

--- src/App.js
+++ src/App.js
@@ -10,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          This is a demo for test auto deploy!!!!!!
+        </p>
       </header>
     </div>
   );
